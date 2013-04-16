14387ec214d2f2e47bcca6c82bb35eab82e0790c
Lesley Oakey <lesley.oakey@gmail.com>
2013-04-16 13:17:06 +0100

    Remove bootstrap

--- index.php
+++ index.php
@@ -4,116 +4,62 @@
     <meta charset="utf-8">
     <title>Super P Bear Page</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/bootstrap-responsive.min.css" rel="stylesheet">
+    <link href="css/init.css" rel="stylesheet">
 
   </head>
 
   <body>
-	<div class="container-fluid">
-	  <div class="row-fluid">
-		<div class="span12">
-			<!-- Logo and title-->
-			<div class="row-fluid">
-				<div class="span12">
+	<div class="container">
+		<div class="header">
+			<div class="content">
+				<h1>Dr P Bear</h1> 
+				<h2>The date is <?php echo date("d-M-Y h:m:s") ?></h2>
+				<h2>Can I connect to MySQL?
+					<?php
+					// Create connection
+					$con=mysqli_connect("localhost","root","sweetpotato","mydatabase");
 
-				<div class="row-fluid">
-						<div class="span12">
-						<h1>Dr P Bear</h1> 
-						<h2>The date is <?php echo date("d-M-Y") ?></h2>
-						<h2>Can I connect to MySQL?
-						<?php
-						// Create connection
-						$con=mysqli_connect("localhost","root","sweetpotato","mydatabase");
-
-						// Check connection
-						if (mysqli_connect_errno($con))
-						{?>
-							<span style="color:red;">NO :( <?php echo mysqli_connect_error() ?></span><?php
-						}
-						else
-						{?>
-							<span style="color:green;">YES :)</span><?php
-						}
-						?>
-						</h2>
-						</div>					
+					// Check connection
+					if (mysqli_connect_errno($con))
+					{?>
+						<span style="color:red;">NO :( <?php echo mysqli_connect_error() ?></span><?php
+					}
+					else
+					{?>
+						<span style="color:green;">YES :)</span><?php
+					}
+					?>
+				</h2>
+			</div>
+		</div>
+		<div class="navigation">
+			<div class="content">
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="#">Secret chocolate secrets</a></li>
+					<li><a href="#">Secret stashes</a></li>
+					<li><a href="#">Recipes</a></li>
+					<li><a href="#">How to get humans to bring you chocolate</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="main">
+			<div class="content">
+				<div class="span5">
+					<img src="img/gallery/headshot1.jpg" width="400" height="400">			
 				</div>
+				<div class="span5">
+					<img src="img/gallery/headshot2.jpg" width="400" height="400">			
 				</div>
 			</div>
 		</div>
-	  </div>
-	</div>
-    <div class="container">
-	
-		<div class="row-fluid">
-				
-		<div class="span2">
-			<h4>P Bear Navigation</h4>
-			<ul>
-				<li>Home</li>
-				<li>About</li>
-				<li>Secret chocolate secrets</li>
-				<li>Secret stashes</li>
-				<li>Recipies</li>
-				<li>How to get humans to bring you chocolate</li>
-			</ul>
-			<h4>Human naviation</h4>
-			<ul>
-				<li>Home</li>
-				<li>About</li>
-				<li>Why P Bears are great</li>
-			</ul>						
-		</div>
-		<div class="span10">
-		  <!-- Main hero unit for a primary marketing message or call to action -->
-			<div class="hero-unit">
-				<div class="row">
-					<div class="span1">		
-					</div>
-					<div class="span5">
-						<img src="img/gallery/headshot1.jpg" width="500" height="500">			
-					</div>
-					<div class="span5">
-						<img src="img/gallery/headshot2.jpg" width="500" height="500">			
-					</div>
-					<div class="span1">		
-					</div>
-				</div>
+		<div class="footer">
+			<div class="content">
+				gfsdegsdrgsd
 			</div>
 		</div>
 	</div>
-
-      <!-- Widget Area , Rows and Columns -->
-      <div class="row">
-        <div class="span4">
-          <h2>Chocolate</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn" href="#">View details &raquo;</a></p>
-        </div>
-        <div class="span4">
-          <h2>Chocolate</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn" href="#">View details &raquo;</a></p>
-       </div>
-        <div class="span4">
-          <h2>Chocolate</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn" href="#">View details &raquo;</a></p>
-        </div>
-      </div>
-
-      <hr>
-
-      <footer>
-        <p>&copy; P Bear 2013</p>
-      </footer>
-
-    </div>
-
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-	<script src="js/bootstrap-min.js"></script>
-
   </body>
-</html>
+</html> 
